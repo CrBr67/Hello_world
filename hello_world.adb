e86e730ca98521f325a898d0884de075b7909c12
CrBr67 <lenaicjacquemin@outlook.fr>
2023-09-26 13:54:32 +0200

    Make more understandable current use of "hello_world"

--- hello_world.adb
+++ hello_world.adb
@@ -5,8 +5,12 @@
 with Ada.Text_IO;
 
 procedure Hello_World is
+   -- Color code list and description available at:
+   -- https://qastack.fr/programming/4842424/list-of-ansi-color-escape-sequences
+   Green_Pencil_Color_Code   : constant String := ASCII.ESC & "[32m";
+   Default_Pencil_Color_Code : constant String := ASCII.ESC & "[0m";
 begin
-   Ada.Text_IO.Put (ASCII.ESC & "[32m");
+   Ada.Text_IO.Put      (Green_Pencil_Color_Code);
    Ada.Text_IO.Put_line ("Hello World !!!");
-   Ada.Text_IO.Put (ASCII.ESC & "[0m");
+   Ada.Text_IO.Put      (Default_Pencil_Color_Code);
 end Hello_World;
